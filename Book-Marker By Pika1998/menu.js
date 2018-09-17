6bbfe9febb957672960ff7504ce1f364b11d0eba
Prafulla1998 <prafzzraichurkar1@gmail.com>
2018-09-17 23:04:18 +0530

    added clear bookmarks option

--- Book-Marker By Pika1998/menu.js
+++ Book-Marker By Pika1998/menu.js
@@ -1,5 +1,6 @@
 document.getElementById("view").addEventListener('click',viewBookMarks);
 document.getElementById("addToBookMarks").addEventListener('click',addToBookMarks);
+document.getElementById("clearAllBookMarks").addEventListener('click',clearBookMarks);
 function viewBookMarks()
 {
   window.open('bookmarker.html','_blank');
@@ -14,4 +15,11 @@ function addToBookMarks()
 function fetchTitle()
 {
 	chrome.runtime.sendMessage("addExternal");
+}
+
+function clearBookMarks()
+{
+	var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+	bookmarks=null;
+	localStorage.setItem('bookmarks',JSON.stringify(bookmarks));
 }
