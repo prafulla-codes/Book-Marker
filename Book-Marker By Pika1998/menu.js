b267963762650ebd83e2ad600a95ddb188ed9e56
Pika1998 <prafzzraichurkar1@gmail.com>
2018-09-23 10:48:10 +0530

    v1.3: Completed UI

--- Book-Marker By Pika1998/menu.js
+++ Book-Marker By Pika1998/menu.js
@@ -2,6 +2,13 @@ document.getElementById("view").addEventListener('click',viewBookMarks);
 document.getElementById("addToBookMarks").addEventListener('click',addToBookMarks);
 document.getElementById("clearAllBookMarks").addEventListener('click',clearBookMarks);
 document.getElementById("addNewCategory").addEventListener('click',addCategory);
+var select = document.getElementById("select_category");
+console.log(select);
+var categories = JSON.parse(localStorage.getItem("categories"));
+for(var i=0;i<categories.length;i++)
+{
+	select.innerHTML+="<option value='"+categories[i]+"'>"+categories[i]+"</option>"
+}
 function viewBookMarks()
 {
   window.open('bookmarker.html','_blank');
@@ -40,11 +47,15 @@ function addCategory()
 		categories=[];
 		categories.push(category_name);
 		localStorage.setItem('categories',JSON.stringify(categories));
+		alert("Sucess! Added New Category :"+category_name);
+		document.getElementById("category_name").value="";
 	}
 	else
 	{
 		categories.push(category_name);
 		localStorage.setItem('categories',JSON.stringify(categories));
+		alert("Sucess! Added New Category :"+category_name);
+		document.getElementById("category_name").value="";
 	}
 	console.log(categories);
 }
